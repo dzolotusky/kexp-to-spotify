9863a9dace92834f89cf58dd9facf728fc726bec
Dave Zolotusky <dzolo@spotify.com>
2020-10-17 09:28:30 +0200

    some minor fixes
    code formatting
    fixed thinking that John or Cheryl are hosts for Street Sounds
    removed some argument parsing code that wasn't used

--- kexp.py
+++ kexp.py
@@ -1,27 +1,17 @@
-"""scrape old playlists from the KEXP API"""
-
-import gzip
-import requests
-import json
 import datetime
-import time
-import argparse
 
+import requests
 import spotipy
 import spotipy.util as sputil
 
-parser = argparse.ArgumentParser(description='Scrape some playlists.')
-parser.add_argument('--start', type=str, help='earliest scrape date', default='')
-parser.add_argument('--end', type=str, help='latest scrape date', default='')
-parser.add_argument('--output_dir', type=str, default='output')
-
-#21 is UTC for 2 PM in Seattle now (it'll be 22 later when Seattle is 8 behind and not 7)
 PLAY_URL = 'https://legacy-api.kexp.org/play/?limit=200&end_time={}&ordering=-airdate&channel=1'
 SHOW_URL = 'https://legacy-api.kexp.org/show/?showid='
+
 
 def make_url(time) -> str:
     """create the URL to get all the plays for a single date"""
     return PLAY_URL.format(time)
+
 
 def plays_for_date(time):
     """fetch all the plays for a single date"""
@@ -29,15 +19,17 @@ def plays_for_date(time):
     response = requests.get(url)
     return response.json()['results']
 
-def scrape_date(time, output_dir: str):
+
+def scrape_date(time):
     plays = plays_for_date(time)
     # `plays` has a lot of repeated info, so we gzip the output files, which
     # gets them about 10x smaller. this probably isn't entirely necessary, but
     # I was running this on an ec2 instance without a lot of disk space.
-    #with gzip.open('{}/{}.txt.gz'.format(output_dir, date.isoformat()), 'w') as f:
+    # with gzip.open('{}/{}.txt.gz'.format(output_dir, date.isoformat()), 'w') as f:
     #    for play in plays:
     #        f.write(bytes(json.dumps(play) + "\n", 'utf-8'))
     return plays
+
 
 def no_john_or_cheryl(cur_show):
     no_john = True;
@@ -53,10 +45,10 @@ def no_john_or_cheryl(cur_show):
     cur_show_name = shows[0]["program"]["name"]
 
     hosts = shows[0]['hosts']
-    print ("show = " + shows[0]["program"]["name"])
+    print("show = " + shows[0]["program"]["name"])
 
-    if cur_show_name == "Street Sounds":
-        return False
+    if "Street Sounds" in cur_show_name:
+        return True
 
     for host in hosts:
         print("host = " + host['name'])
@@ -65,15 +57,16 @@ def no_john_or_cheryl(cur_show):
 
     return no_john
 
-if __name__ == "__main__":
-    token = sputil.prompt_for_user_token("dzolotusky", client_id='1310a11ef19c4ae29de1a51bddbba54d', client_secret='fee84ea8154a4e0fbc050a29aa55a46c', redirect_uri='http://www.kexp.org', scope='playlist-modify-public')
-    sp = spotipy.Spotify(auth=token)
 
-    args = parser.parse_args()
+if __name__ == "__main__":
+    token = sputil.prompt_for_user_token("", client_id='',
+                                         client_secret='',
+                                         redirect_uri='http://www.kexp.org', scope='playlist-modify-public')
+    sp = spotipy.Spotify(auth=token)
 
     plays = []
 
-    month_string = '2018-03-'
+    month_string = '2020-10-'
 
     playlists = {}
 
@@ -82,7 +75,8 @@ if __name__ == "__main__":
     for i in range(10, 18):
         date = month_string + str(i)
         print("reading data for {}".format(date))
-        plays = scrape_date(date + "T" + "23:15:00" + "Z", args.output_dir)
+        # 21 is UTC for 2 PM in Seattle now (it'll be 22 later when Seattle is 8 behind and not 7)
+        plays = scrape_date(date + "T" + "22:15:00" + "Z")
 
         cur_show = 0
         cur_show_name = None
@@ -96,14 +90,14 @@ if __name__ == "__main__":
             is_monday = 0 == datetime.datetime.strptime(last_play['airdate'], "%Y-%m-%dT%H:%M:%SZ").weekday()
             last_is_not_john = no_john_or_cheryl(last_play['showid'])
             if (not last_is_not_john) or is_monday:
-                plays.extend(scrape_date(last_play['airdate'], args.output_dir))
+                plays.extend(scrape_date(last_play['airdate']))
             else:
                 print("verified that last track isn't John's or Cheryl's")
 
         prev_url = None
         for play in plays:
             if (play['playid'] in visited_plays):
-                print ("skipping play number:" + str(play['playid']))
+                print("skipping play number:" + str(play['playid']))
                 continue
 
             visited_plays.append(play['playid'])
@@ -124,8 +118,8 @@ if __name__ == "__main__":
                     print("Show with " + hosts[0]['name'] + " " + str(cur_show))
 
                 if prev_show == 0:
-                    assert(no_john)
-                    print ("verified that first track isn't John's or Cheryl's")
+                    assert (no_john)
+                    print("verified that first track isn't John's or Cheryl's")
 
             if (not no_john and play['artist'] and play['track']):
                 query_string = 'artist:' + play['artist']['name'] + ' track:' + play['track']['name']
@@ -133,9 +127,9 @@ if __name__ == "__main__":
                 if len(results['tracks']['items']) > 0:
                     track = results['tracks']['items'][0]
                     if prev_url == track['uri']:
-                        print ("just saw " + track['name'] + ' ' + track['uri'])
+                        print("just saw " + track['name'] + ' ' + track['uri'])
                     else:
-                        print (' ', track['name'] + ' ' + track['uri'])
+                        print(' ', track['name'] + ' ' + track['uri'])
                         playlists[cur_show_name].append(track['uri'])
                         prev_url = track['uri']
                 else:
@@ -149,7 +143,7 @@ if __name__ == "__main__":
             playlist = sp.user_playlist_create('dzolotusky', 'KEXP - ' + kexp_playlist, True)
             playlists[kexp_playlist].reverse()
             sp.user_playlist_add_tracks(playlist_id=playlist['id'], user='dzolotusky', tracks=playlists[kexp_playlist])
-            print ('Created ' + playlist['id'] + 'with ' + str(len(playlists[kexp_playlist])) + ' tracks')
+            print('Created ' + playlist['id'] + ' with ' + str(len(playlists[kexp_playlist])) + ' tracks')
             print("Found " + str(len(playlists[kexp_playlist])) + " tracks for " + kexp_playlist)
         else:
             print('Did not create playlist, no tracks found to add')
